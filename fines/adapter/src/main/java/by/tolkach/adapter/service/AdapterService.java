6184834d7bfd7b8a2a0cc3f710a859a963a2fc53
AleksandrTolkach <shuuurick@mail.ru>
2023-01-08 18:42:34 +0300

    create BAD solution

--- fines/adapter/src/main/java/by/tolkach/adapter/service/AdapterService.java
+++ fines/adapter/src/main/java/by/tolkach/adapter/service/AdapterService.java
@@ -13,7 +13,6 @@ import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 @Service
 public class AdapterService implements IAdapterService {
